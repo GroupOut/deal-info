f1779de5119b213201bcddacb8ca113cbb06eb01
Nolan Yee <skinnyfatman@gmail.com>
2018-07-31 16:33:27 -0700

    Committing changes to unitTesting, some proxy modifications may have been mixed in.

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,22 @@
-let lieDetector = function(){
-  return true;
-}
+import React from 'react';
+import {shallow, mount, render} from 'enzyme';
 
-test('true still equals true', () => {
-  expect(lieDetector('Everything I say is a lie')).toBe(true);
-});
+import Main from '../client/components/Main.jsx';
+
+// let lieDetector = function(){
+//   return true;
+// }
+
+describe('<Main />', () => {
+  it('should be defined', () => {
+    expect(Main).toBeDefined();
+  })
+  it('should render', () => {
+    let mainWrap = shallow(<Main />)
+    console.log(mainWrap);
+  })
+})
+
+// test('true still equals true', () => {
+//   expect(lieDetector('Everything I say is a lie')).toBe(true);
+// });
